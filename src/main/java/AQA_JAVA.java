ad5453b4c9e5c363db2b150f9e831601ea58bafa
sowlanation <sadalex99@gmail.com>
2022-04-10 21:51:48 +0300

    Реализовала первый урок

--- src/main/java/AQA_JAVA.java
+++ src/main/java/AQA_JAVA.java
@@ -1,5 +1,64 @@
+import java.util.Scanner;
+
 public class AQA_JAVA {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Task 2");
+        System.out.println("Orange\nBanana\nApple");
+        System.out.println("/////////////////////////////");
+    }
+
+    public static void checkSumSign() {
+        System.out.println("Task 3");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите значение а: ");
+        int a = s.nextInt();
+        System.out.println("Введите значение b: ");
+        int b = s.nextInt();
+        int c = a + b;
+        System.out.print("Результат: ");
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+        System.out.println("/////////////////////////////");
+    }
+
+    public static void printColor() {
+        System.out.println("\nTask 4");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите значение а: ");
+        int value = s.nextInt();
+        System.out.print("Результат: ");
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+        System.out.println("/////////////////////////////");
+    }
+
+    public static void compareNumbers() {
+        System.out.println("\nTask 5");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите значение а: ");
+        int a = s.nextInt();
+        System.out.println("Введите значение b: ");
+        int b = s.nextInt();
+        System.out.print("Результат: ");
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
     }
 }

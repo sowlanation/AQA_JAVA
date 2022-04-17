7a4114806a104877008f4a6246c451e176683893
sowlanation <sadalex99@gmail.com>
2022-04-17 13:47:14 +0300

    Реализовала ДЗ_5

---
+++ src/main/java/lesson_5_task_2/Apple.java
@@ -0,0 +1,8 @@
+package lesson_5_task_2;
+
+public class Apple extends Fruit {
+    @Override
+    float weight() {
+        return 1.0f;
+    }
+}
---
+++ src/main/java/lesson_5_task_2/Fruit.java
@@ -0,0 +1,5 @@
+package lesson_5_task_2;
+
+public abstract class Fruit {
+    abstract float weight();
+}
